fde3989ef945a8de1caeed178866e23d3c6d841b
Rishi Gupta <rishi.gupta@sciex.com>
2021-04-04 23:24:55 +0530

    fix

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,7 +4,7 @@ import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
 import classes from './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux/Aux';
+import Aux from '../../../hoc/Auxi/Auxi';
 
 const sideDrawer = (props) => {
   console.log('[sideDrawer]');
